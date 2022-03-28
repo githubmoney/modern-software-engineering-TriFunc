1304965cf318f85fc952459f7b959730e03e9990
snow <97205517+px129@users.noreply.github.com>
2022-03-28 16:27:04 +0800

    Add files via upload

--- Func_arctan.py
+++ Func_arctan.py
@@ -1,23 +1,43 @@
-pi = 3.1415926535897932
-def arctan(x):
-        if x > 1 and x < -1:
-            return print("幅度和角度分别是：", None)
-        else:
-            if x >= -1 and x <= 1:
-                x = x
-                n = 0
-                m = 0
-                while (n < 100):
-                    m += (-1) ** n * x ** (2 * n + 1) / (2 * n + 1)
-                    n = n + 1
-                result = m * 180 / pi
-                m = format(m, '.9f')
-                w = format(result, '.9f')
-                return m, w
-    #return m
-m = float(input('请输入x:'))
-print("幅度和角度分别是：", arctan(m))
+from math import pi
+from math import atan
 
-"""print("输出幅度：", format(arctan(m), '.9f'))
-result = arctan(m) * 180 / pi
-print("输出角度:", format(result, '.9f'))"""
+
+def arctan(input):
+    x1 = input
+    delta = 0.000000001
+    n = 0
+    y = 0
+    # 当x绝对值小于1时用泰勒展开进行计算
+    if x1 >= 0 and x1 <= 1:
+        while x1 ** (2 * n + 1) / (2 * n + 1) >= delta:
+            y += (-1) ** n * x ** (2 * n + 1) / (2 * n + 1)
+            n += 1
+    elif x1 <= 0 and x1 >= -1:
+        x2 = -x1
+        while x2 ** (2 * n + 1) / (2 * n + 1) >= delta:
+            y += (-1) ** n * x2 ** (2 * n + 1) / (2 * n + 1)
+            n += 1
+        y = -y
+    # 当x绝对值大于1时，arctanx=pi/2-arctan(1/x)
+    elif x > 1:
+        x3 = 1 / x1
+        while x3 ** (2 * n + 1) / (2 * n + 1) >= delta:
+            y += (-1) ** n * x3 ** (2 * n + 1) / (2 * n + 1)
+            n += 1
+        y = pi / 2 - y
+    else:
+        x4 = 1 / -x1
+        while x4 ** (2 * n + 1) / (2 * n + 1) >= delta:
+            y += (-1) ** n * x4 ** (2 * n + 1) / (2 * n + 1)
+            n += 1
+        y = pi / 2 - y
+        y = -y
+    return format(y, '.9f')
+
+
+x = float(input('请输入x:'))
+print("自定义函数输出结果：", arctan(x))
+print("库函数输出结果：", atan(x))
+
+
+
