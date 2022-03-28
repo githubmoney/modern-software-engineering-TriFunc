68354a8bca0acb3d33f24b151ab7f9ffebe7b9b8
snow <97205517+px129@users.noreply.github.com>
2022-03-28 11:12:32 +0800

    Add files via upload

--- Angel2Radian.py
+++ Angel2Radian.py
@@ -1,16 +1,16 @@
-# import math
-PI = 3.141592653589793
+# from math import degrees, radians
+from math import pi
 
 
 # 角度转弧度函数
 def angel2radian(angel):
-    radian = angel * PI / 180
+    radian = angel * pi / 180
     return radian
 
 
 # 弧度转角度函数
 def radian2angel(radian):
-    angel = radian * 180 / PI
+    angel = radian * 180 / pi
     return angel
 
 
@@ -22,12 +22,12 @@ if choose_num == 1:
     angel_input = float(input("请输入一个角度："))
     radian_output = angel2radian(angel_input)
     print("自定义函数计算结果：", radian_output)
-#    print("库函数计算结果：", math.radians(angel_input))  # 调用库函数测试自定义函数运算结果
+#    print("库函数计算结果：", radians(angel_input))  # 调用库函数测试自定义函数运算结果
 elif choose_num == 2:
     # 输入弧度转换为角度并输出
     radian_input = float(input("请输入一个弧度："))
     angel_output = radian2angel(radian_input)
     print("自定义函数计算结果：", angel_output)
-#    print("库函数计算结果：", math.degrees(radian_input))  # 调用库函数测试自定义函数运算结果
+#    print("库函数计算结果：", degrees(radian_input))  # 调用库函数测试自定义函数运算结果
 else:
     print("输入错误！！！")
