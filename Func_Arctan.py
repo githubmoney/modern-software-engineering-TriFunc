8ffea38acc997af57438f6ed6470eb8390c9266f
snow <97205517+px129@users.noreply.github.com>
2022-03-28 17:39:29 +0800

    Update Func_Arctan.py

--- Func_Arctan.py
+++ Func_Arctan.py
@@ -8,18 +8,12 @@ def arctan(input):
     n = 0
     y = 0
     # 当 x = -1 或者 x = 1时用泰勒展开进行计算
-    if (input == -1) | (input == 1):
-        t = input
-        n = 0
-        m = 0
-        while n < 100:
-            m += (-1) ** n * t ** (2 * n + 1) / (2 * n + 1)
+    if (x1 == -1) | (x1 == 1):
+        while n < 100000:
+            m += (-1) ** n * x1 ** (2 * n + 1) / (2 * n + 1)
             n = n + 1
-        # result = m * 180 / pi # 可以转角度
-        m = format(m, '.9f')
-        # w = format(result, '.9f') # 转角度输出
-        return m
-    if (x1 >= 0) and (x1 < 1):
+    # 当x绝对值小于1时用泰勒展开进行计算
+    elif (x1 >= 0) and (x1 < 1):
         while x1 ** (2 * n + 1) / (2 * n + 1) >= delta:
             y += (-1) ** n * x ** (2 * n + 1) / (2 * n + 1)
             n += 1
