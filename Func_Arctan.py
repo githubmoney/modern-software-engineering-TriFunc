73bd44d9c3111aa7524b9b9e5779363b1a29cf56
李慧 <101334907+LiHui0408@users.noreply.github.com>
2022-03-30 11:53:33 +0800

    Add files via upload

--- Func_Arctan.py
+++ Func_Arctan.py
@@ -5,43 +5,41 @@ pi = 3.141592653
 
 
 def arctan(in1):
-    x1 = in1
     delta = 0.000000001
     n = 0
     y = 0
-    # 当x绝对值小于1时用泰勒展开进行计算
-    if (x1 == -1) | (x1 == 1):
-        t = x1
+    # 当 |x| = 1时用泰勒展开进行计算
+    if (in1 == -1) | (in1 == 1):
+        t = in1
         n = 0
         m = 0
         while n < 100:
             m += (-1) ** n * t ** (2 * n + 1) / (2 * n + 1)
             n = n + 1
         # result = m * 180 / pi # 可以转角度
-        m = format(m, '.9f')
         # w = format(result, '.9f') # 转角度输出
         return m
-    if (x1 >= 0) and (x1 < 1):
+    if (in1 >= 0) and (in1 < 1):
+        while in1 ** (2 * n + 1) / (2 * n + 1) >= delta:
+            y += (-1) ** n * in1 ** (2 * n + 1) / (2 * n + 1)
+            n += 1
+    elif (in1 <= 0) and (in1 > -1):
+        x1 = -in1
         while x1 ** (2 * n + 1) / (2 * n + 1) >= delta:
             y += (-1) ** n * x1 ** (2 * n + 1) / (2 * n + 1)
             n += 1
-    elif (x1 <= 0) and (x1 > -1):
-        x2 = -x1
+        y = -y
+    # 当|x| > 1时，arctanx=pi/2-arctan(1/x)
+    elif in1 > 1:
+        x2 = 1 / in1
         while x2 ** (2 * n + 1) / (2 * n + 1) >= delta:
             y += (-1) ** n * x2 ** (2 * n + 1) / (2 * n + 1)
             n += 1
-        y = -y
-    # 当x绝对值大于1时，arctanx=pi/2-arctan(1/x)
-    elif x1 > 1:
-        x3 = 1 / x1
-        while x3 ** (2 * n + 1) / (2 * n + 1) >= delta:
-            y += (-1) ** n * x3 ** (2 * n + 1) / (2 * n + 1)
-            n += 1
         y = pi / 2 - y
     else:
-        x4 = 1 / -x1
-        while x4 ** (2 * n + 1) / (2 * n + 1) >= delta:
-            y += (-1) ** n * x4 ** (2 * n + 1) / (2 * n + 1)
+        x3 = 1 / -in1
+        while x3 ** (2 * n + 1) / (2 * n + 1) >= delta:
+            y += (-1) ** n * x3 ** (2 * n + 1) / (2 * n + 1)
             n += 1
         y = pi / 2 - y
         y = -y
@@ -55,7 +53,6 @@ def ku(in2):
 
 
 def test(angle):
-
     myArctan = arctan(angle)
     systemArctan = math.atan(angle)
     print(myArctan)
@@ -72,4 +69,3 @@ for i in range(1, 100):
     if ans > 0.0001:
         flag = False
     print('input : %.5f' % x, "minus", '%.9f' % ans, flag)
-
