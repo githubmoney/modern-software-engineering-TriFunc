1f725ed9f38321fe43c9a93e04a8696308f40f9f
郝聪宇 <101335052+hcy0304@users.noreply.github.com>
2022-03-30 17:24:38 +0800

    Add files via upload

--- func_arcsin.py
+++ func_arcsin.py
@@ -1,10 +1,12 @@
 from decimal import Decimal
+import numpy as np
+import random
 def arcsinx(x):
      x=float(x)
      #arcsin定义域为[-1,1]，大于1或者小于-1超出定义域
      if x>1 or x<-1:
          #print('错误，请输入-1到1间的数')
-          return print('错误，请输入-1到1间的数')
+          return None
 
      if x>=0 and x<=1:
           arcsinx=0
@@ -15,7 +17,7 @@ def arcsinx(x):
           while (Decimal(factorial(2*n))*Decimal(x)**Decimal((2*n+1))/(4**Decimal(n)*Decimal(factorial(n))*Decimal(factorial(n))*Decimal((2*n+1))) >= delta):
                 arcsinx += Decimal(factorial(2*n))*Decimal(x)**Decimal((2*n+1))/(4**Decimal(n)*Decimal(factorial(n))*Decimal(factorial(n))*Decimal((2*n+1)))
                 n = n + 1
-          return print(format(arcsinx, '.9f'))
+          return format(arcsinx, '.9f')
      if x>=-1 and x<0:
           arcsinx = 0
           n = 0
@@ -27,7 +29,7 @@ def arcsinx(x):
                 arcsinx += Decimal(factorial(2*n))*Decimal(x)**Decimal((2*n+1))/(4**Decimal(n)*Decimal(factorial(n))*Decimal(factorial(n))*Decimal((2*n+1)))
                 n = n + 1
           arcsinx = -arcsinx
-          return print(format(arcsinx, '.9f'))
+          return format(arcsinx, '.9f')
 
 def factorial(m):    #阶乘
      if m == 0:
@@ -37,5 +39,20 @@ def factorial(m):    #阶乘
          for i in range(2,m+1):
              result = result*i
          return result
-x = float(input('请输入x:'))
-y = arcsinx(x)
+
+def test(angle):
+    myArcsinx = float(arcsinx(angle))
+    systemArcsinx = np.arcsin(angle)
+
+    minus =systemArcsinx-myArcsinx
+
+    return minus
+
+
+for i in range(1, 100):
+    x = random.uniform(-1, 1)
+    ans = test(x)
+    flag = True
+    if ans > 0.001:
+        flag = False
+    print('input : %.5f' % x, "minus", '%.9f' % ans, flag)
